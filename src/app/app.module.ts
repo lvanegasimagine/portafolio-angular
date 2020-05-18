a6b589c87bf64bb97cfcf6eef20f9cb8787d8327
Luis Vanegas <lvanegas1429@gmail.com>
2020-05-18 14:50:50 -0600

    Fin de la Seccion 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+import { SearchComponent } from './pages/search/search.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { ItemComponent } from './pages/item/item.component';
     HeaderComponent,
     PortafolioComponent,
     AboutComponent,
-    ItemComponent
+    ItemComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { rejects } from 'assert';
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +9,52 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-8f41f.firebaseio.com/productos_idx.json')
-    .subscribe((resp: Producto[]) => {
-      console.log(resp);
-      this.productos = resp;
-      this.cargando = false;
+
+    return new Promise( (resolve, reject ) => {
+        this.http.get('https://angular-html-8f41f.firebaseio.com/productos_idx.json')
+        .subscribe((resp: Producto[]) => {
+          this.productos = resp;
+          this.cargando = false;
+          resolve();
+        });
     });
   }
-  
   getProducto(idProducto: string){
     return this.http.get(`https://angular-html-8f41f.firebaseio.com/productos/${idProducto}.json`);
   }
+  buscarProducto(termino: string){
 
+    if(this.productos.length === 0){
+      /// cargar productos
+      this.cargarProductos().then(() => {
+          /// Ejecutar despues de tener los productos
+          /// Aplicar el filtro
+          this.filtrarProductos(termino);
+      });
+    }
+    else{
+      /// aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+  }
+
+  private filtrarProductos(termino: string){
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if (prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0){
+        this.productosFiltrado.push(prod);
+      }
+    });
+  }
 }
